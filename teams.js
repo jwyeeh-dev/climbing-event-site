669ebe6029ebd4a842a0e3b2862f0d2c8c461c5e
Jaeyeong Hwang <jwyeeh@gmail.com>
2025-07-10 14:58:01 +0900

    change: team names are encoded & searching available

--- teams.js
+++ teams.js
@@ -1,1 +1,59 @@
-const encodedTeams = 'W3sidGVhbSI6ICJz7Iah7Y+IIiwgIm1lbWJlcnMiOiBbIuq5gOyjvOyDneyCsOuhnCIsICLrsJjseonrr7giLCAi6rmA7Jqp7ZWYIl19LCB7InRlYW0iOiAxLCAibWVtYmVycyI6IFsi7IaM7Zi467aUIiwgIuydgOynhO2YhCIsICLrsJjtnIzrp4siXX0sIHsidGVhbSI6IDIsICJtZW1iZXJzIjogWyLsl6zthZzqtaEiLCAi64uI7KeA7JuUIiwgIu2ZlOybkOydgCJdfSwgeyJ0ZWFtIjogMywgIm1lbWJlcnMiOiBbIu2FjOqzoOyasCIsICLtlZzslrTrn6UiLCAi7Jes7Ji87IqIIl19LCB7InRlYW0iOiA0LCAibWVtYmVycyI6IFsi7ISc7KeA7JWEIiwgIu2FjOynhOuhnCIsICLsl6zthZzslrAiXX0sIHsidGVhbSI6IDUsICJtZW1iZXJzIjogWyLsl6zslrDsiJgiLCAi7KG47LC97ZmA7J2QiLCAi64uI67O07Zi4Il19LCB7InRlYW0iOiA2LCAibWVtYmVycyI6IFsi7J2R7Iqk7JuQIiwgIuydgOyjvOuvuCIsICLri4jri6Tsl6AiXX1d';
+// teams.js
+const teamsData = [
+  {
+    team: "스태프",
+    members: [
+      "7Iug7KKF7Jqx",   
+      "67CV7ISg66+8",  
+      "7Iug7Jqp7ZWY"    
+    ]
+  },
+  {
+    team: "1조",
+    members: [
+      "7IaM7ZiE67mI",   
+      "7J6E7LGE7ZiE",   
+      "67CV7Zic66+8"   
+    ]
+  },
+  {
+    team: "2조",
+    members: [
+      "7J207IaM7KeE", 
+      "6rmA7KeA7JuQ",   
+      "7Zmp7J6s7JiB" 
+    ]
+  },
+  {
+    team: "3조",
+    members: [
+      "6rmA7IOB7Jqw", 
+      "7ZWc7Jyg64KY", 
+      "7J207JiI7Iqs"   
+    ]
+  },
+  {
+    team: "4조",
+    members: [
+      "7ISc7KeA7JWE",  
+      "6rmA7LGE66Gd",   
+      "7J207IaM7JiB"    
+    ]
+  },
+  {
+    team: "5조",
+    members: [
+      "7Jik7JiB7Iug",   
+      "7KGw7LC97Z2s",   
+      "6rmA67O07ZiE"   
+    ]
+  },
+  {
+    team: "6조",
+    members: [
+      "7Jyg7Iq57JuQ",   
+      "7J207KCV66+8", 
+      "6rmA64uk7J2A"   
+    ]
+  }
+];
